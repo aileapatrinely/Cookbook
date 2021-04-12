35667ce6660faec0b9548e895692830296523c53
Ailea Patrinely <aileapatrinely@gmail.com>
2021-04-12 12:23:15 -0500

    provides database table info

--- database.sql
+++ database.sql
@@ -8,3 +8,22 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "recipe" (
+	"id" SERIAL PRIMARY KEY,
+	"recipe_name" VARCHAR (100) UNIQUE NOT NULL,
+	"user_id" INT REFERENCES "user" ON DELETE CASCADE
+	);
+	
+CREATE TABLE "ingredients" (
+	"id" SERIAL PRIMARY KEY,
+	"ingredient_name" VARCHAR (100) NOT NULL,
+	"amount" VARCHAR (80) NOT NULL,
+	"recipe_id" INT REFERENCES "recipe" ON DELETE CASCADE
+	);
+	
+CREATE TABLE "instructions" (
+	"id" SERIAL PRIMARY KEY,
+	"instructions" VARCHAR (1000) NOT NULL,
+	"recipe_id" INT REFERENCES "recipe" ON DELETE CASCADE
+	);
