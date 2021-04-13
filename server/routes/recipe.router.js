b5f7f8d4bf8419095544e25e0ad387ec004c48db
Ailea Patrinely <aileapatrinely@gmail.com>
2021-04-13 12:03:07 -0500

    creates GET for recipes

--- server/routes/recipe.router.js
+++ server/routes/recipe.router.js
@@ -6,7 +6,11 @@ const router = express.Router();
  * GET route template
  */
 router.get('/', (req, res) => {
-    
+    const queryText = `SELECT * FROM "recipe" WHERE "recipe".user_id = $1;`;
+    pool
+        .query(queryText, [req.user.id])
+        .then ((response) => res.send(response.rows))
+        .catch((error) => console.log('Error in recipe GET route', error));
 });
 
 /**
