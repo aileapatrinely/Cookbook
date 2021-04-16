66b7f1dbe7f10d5b314ae3bb0b47c450e8e8f4b1
Ailea Patrinely <aileapatrinely@gmail.com>
2021-04-16 10:24:57 -0500

    imports routes into server.js

--- server/server.js
+++ server/server.js
@@ -10,6 +10,9 @@ const passport = require('./strategies/user.strategy');
 
 // Route includes
 const userRouter = require('./routes/user.router');
+const recipeRouter = require('./routes/recipe.router');
+const ingredientsRouter = require('./routes/ingredients.router');
+const instructionsRouter = require('./routes/instructions.router');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -24,6 +27,9 @@ app.use(passport.session());
 
 /* Routes */
 app.use('/api/user', userRouter);
+app.use('/api/recipe', recipeRouter);
+app.use('/api/ingredients', ingredientsRouter);
+app.use('/api/instructons', instructionsRouter);
 
 // Serve static files
 app.use(express.static('build'));
