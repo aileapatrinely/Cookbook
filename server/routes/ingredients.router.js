866c32cb940dc1910cfc4a5002d5395e55db625b
Ailea Patrinely <aileapatrinely@gmail.com>
2021-04-16 09:02:49 -0500

    creates GET for ingredients

--- server/routes/ingredients.router.js
+++ server/routes/ingredients.router.js
@@ -6,7 +6,15 @@ const router = express.Router();
  * GET route template
  */
 router.get('/', (req, res) => {
-    
+    const recipe_id = req.body.recipe_id;
+
+    const queryText = `SELECT * FROM "ingredients" WHERE "ingredients".recipe_id=$1;`;
+    pool
+        .query(queryText, [
+            recipe_id,
+    ])
+    .then(() => res.sendStatus(201))
+    .catch(() => res.sendStatus(500));
 });
 
 /**
